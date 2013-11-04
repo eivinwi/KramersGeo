9befa5def239c6f72879e8e65ce6ce2ee996bd3d
André Kramer Orten <andrekor@andrekor-VPCS12X9E.(none)>
2013-11-04 11:47:00 +0100

    lagt til index.html

---
+++ KramersGeo/src/main/java/no/uio/inf5750/KramersGeo/App.java
@@ -0,0 +1,13 @@
+package no.uio.inf5750.KramersGeo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

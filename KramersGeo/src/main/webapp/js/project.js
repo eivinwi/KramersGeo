bd06aa6b4a0a98ec499ae450c59e3223070acec8
Kenneth Klette Jonassen <kennetkl@ifi.uio.no>
2013-11-22 16:08:05 +0100

    Add map interactivity

--- KramersGeo/src/main/webapp/js/project.js
+++ KramersGeo/src/main/webapp/js/project.js
@@ -1,25 +1,96 @@
 'use strict';
 
-function initialize_map()
+var GoogleAPIKey = 'AIzaSyDY7GeWGMJ7CiH2okMABZ3HBF9Fx6FXZg8';
+
+function initialize_gmaps()
 {
-   var mapOptions = {
+  var acOptions = {
+    componentRestrictions: {country: 'no'}
+  };
+
+  var mapOptions = {
     zoom: 8,
     minZoom: 3,
     center: new google.maps.LatLng(59.923022,10.752869),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
+  var ac = new google.maps.places.Autocomplete($('#location')[0], {});
   var map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+  var gc = new google.maps.Geocoder();
+  //var places = new google.maps.places.PlacesService(map);
+  var marker = null;
+  
+  var setLocation = function (latLng) {
+    var lat = latLng.ob,
+      lng = latLng.pb;
+    
+    $('#lat').val(lat)
+    $('#lng').val(lng)
+
+    if (marker != null) {
+      marker.setVisible(false)
+      marker.setMap(null)
+    }
+    
+    marker = new google.maps.Marker({
+      position: latLng,
+      map: map,
+      animation: google.maps.Animation.DROP
+    });
+    
+  };
+
+  google.maps.event.addListener(ac, 'place_changed', function() {
+    var place = ac.getPlace();
+    
+    setLocation(place.geometry.location);
+  });
+  
+  google.maps.event.addListener(map, 'rightclick', function(ev) {
+    /*
+    var deltaLatitude  = ev.ca.y /  111111;
+    var deltaLongitude = ev.ca.x / (111111 * Math.cos(ev.latLng.pb));
+    var sw = new google.maps.LatLng(
+      ev.latLng.lat() - deltaLatitude  / 2,
+      ev.latLng.lng() - deltaLongitude / 2);   
+    var ne = new google.maps.LatLng(
+      ev.latLng.lat() + deltaLatitude  / 2,
+      ev.latLng.lng() + deltaLongitude / 2);   
+    
+    var extent = new google.maps.LatLngBounds(sw, ne)
+    */
+    var gcReq = {
+/*      bounds: extent, */
+      location: ev.latLng
+    }
+
+    setLocation(ev.latLng);
+    gc.geocode(gcReq, function (results, status) {
+      console.log(status)
+      console.log(results)
+      $('#location').val(results[0].formatted_address);
+    });
+    
+    
+    if ($('#autohide:checked').length) {
+    	$('body').removeClass('map');
+      $('#map').val(0);
+    	$('#map').removeClass('active');
+    }
+  });
 }
 
 var add_map = function()
 {
   var script = document.createElement('script');
   script.type = 'text/javascript';
-  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-      'key=AIzaSyDY7GeWGMJ7CiH2okMABZ3HBF9Fx6FXZg8&' +
-      'callback=initialize_map';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+      'libraries=places&' +
+      'sensor=false&' +
+      'key=' + GoogleAPIKey +
+      '&callback=initialize_gmaps';
   document.body.appendChild(script);
 }
 
@@ -51,7 +122,7 @@ $(function() {
   });
 
   $('#map').click(function(ev) {
-  	$('body').toggleClass('map');
+  	$('body').toggleClass('map', $(this).val());
   });
 
   add_map();

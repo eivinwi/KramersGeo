f32f739172139d9b6b445aa388769fa39db60753
Eivind Wikheim <eivinwi@student.matnat.uio.no>
2013-12-05 11:54:38 +0100

    .

--- KramersGeo/src/main/webapp/js/project.js
+++ KramersGeo/src/main/webapp/js/project.js
@@ -228,7 +228,7 @@ function sendEvent(data) {
 		headers: {
 			Authorization : "Basic " + btoa(user+":"+password)
 		},
-		data: data,
+		data: data, //JSON.stringify?
 		success: function() {
 			console.log("Sent event");
 		},

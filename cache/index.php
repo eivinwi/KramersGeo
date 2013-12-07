aa7e897ad38f9bd3d36e852069ead35dc50c17a1
Kenneth Klette Jonassen <kennetkl@ifi.uio.no>
2013-12-07 21:08:02 +0100

    Intermediate API proxy with client-side caching support (not for production)
    Serves 2 functions:
     - Enables HTTP authentication to the DHIS API when code resides on
       some other domain than apps.dhis2.org.
     - Enables web clients to cache data returned from the DHIS2 API
       via adding ETag-headers in response. Clients will still re-validate
       their locally, cached version with the proxy on each request. However,
       it's both feasible and recommended to add certain rules for which data
       the client may cache without re-validating. E.g. it would be reasonable
       for clients to keep the optionset of ICD-codes without revalidating their
       copy for several days, months, ...

---
+++ cache/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("ob_etag_handler.php");
+ob_start("ob_etag_handler");
+header("Access-Control-Allow-Origin: *");
+
+$ch = curl_init();
+
+function url_get_contents ($url) {
+  global $ch;
+
+  if (!$ch) {
+    return null;
+  }
+
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+
+  $data = curl_exec($ch);
+  return $data;
+}
+
+/*
+ * DHIS Developer API has an invalid HTTPS certificate at time of writing.
+ */
+$username = rawurlencode($_SERVER['PHP_AUTH_USER']);
+$password = rawurlencode($_SERVER['PHP_AUTH_PW']);
+
+$base = "http://$username:$password@apps.dhis2.org/dev/api";
+$ext  = substr($_SERVER['REQUEST_URI'], strlen($_SERVER['SCRIPT_NAME']));
+
+header("Content-type: application/json");
+echo url_get_contents($base . $ext);
+
+curl_close($ch);
